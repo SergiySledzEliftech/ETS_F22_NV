26063c747ad5e595463a4815c5bbb6919890112f
Serhii <havryliuk.serhii@icloud.com>
2022-06-28 21:07:50 +0300

    add basic components

--- components/TestComponent.vue
+++ components/TestComponent.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
-    <p> Test component </p>
+    <p> Basic components </p>
+    <h1> Title h1</h1>
+    <h2> Title h2 </h2>
+    <h3> Title h3 </h3>
+    <h4> Title h4 </h4>
+    <h5> Title h5 </h5>
+    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dignissim scelerisque nisi a imperdiet. Ut malesuada vel nunc id suscipit. Nullam dapibus orci a orci faucibus aliquet.</p>
+    <a href="#"> Link to</a>
   </div>
 </template>
 
