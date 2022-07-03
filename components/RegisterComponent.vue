c41d5d6ba679924a12f6657f5ef0e31abeb8eb02
unknown <libraolena@gmail.com>
2022-07-03 22:25:49 +0300

    login form

---
+++ components/RegisterComponent.vue
@@ -0,0 +1,144 @@
+<template>
+  <v-app>
+    <v-form ref="form" v-model="valid" lazy-validation>
+      <v-text-field
+        v-model="firstName"
+        :rules="firstNameRules"
+        label="First Name"
+        type="text"
+        required
+        class="input-form mb-6"
+        solo
+      />
+
+      <v-text-field
+        v-model="lastName"
+        :rules="lastNameRules"
+        label="Last Name"
+        type="text"
+        required
+        class="input-form mb-6"
+        solo
+      />
+
+      <v-text-field
+        v-model="email"
+        :rules="emailRules"
+        label="E-mail"
+        type="text"
+        required
+        class="input-form mb-6"
+        solo
+      />
+
+      <v-text-field
+        v-model="password"
+        :append-icon="show ? 'visibility' : 'visibility_off'"
+        :rules="passwordRules"
+        :type="show ? 'text' : 'password'"
+        :counter="8"
+        label="Password"
+        required
+        class="input-form mb-2"
+        solo
+        @click:append="show = !show"
+      />
+      <v-checkbox
+        v-model="ex4"
+        label="I agree with rules"
+        color="#E31F26"
+        value="red"
+        hide-details
+        class="mb-2"
+      />
+      <v-btn :disabled="!valid" class="btn_primary mt-4" width="100%" @click="validate">
+        Sign Up
+      </v-btn>
+      <!--
+        <v-btn color="error" class="mr-4" @click="reset">
+          Reset Form
+        </v-btn>
+
+        <v-btn color="warning" @click="resetValidation">
+          Reset Validation
+        </v-btn> -->
+    </v-form>
+  </v-app>
+</template>
+<script>
+import { Component, Vue } from 'nuxt-property-decorator'
+
+export default
+  @Component({
+    name: 'RegisterComponent'
+  })
+
+class RegisterComponent extends Vue {
+    valid = true;
+    firstName = '';
+    lastName = '';
+    password = '';
+    show = false;
+    email = '';
+    firstNameRules = [
+      v => !!v || 'First name is required',
+      v => (v && v.length <= 10) || 'First name must be valid'
+    ];
+
+    lastNameRules = [
+      v => !!v || 'Last Name is required',
+      v => (v && v.length <= 10) || 'Last name must be valid'
+    ];
+
+    emailRules = [
+      v => !!v || 'E-mail is required',
+      v => /.+@.+\..+/.test(v) || 'E-mail must be valid'
+    ];
+
+    passwordRules = [
+      v => !!v || 'Password is required',
+      v => (v && v.length >= 8) || 'Password must be at least 8 characters'
+    ];
+
+    validate () {
+      this.$refs.form.validate()
+    }
+
+    reset () {
+      this.$refs.form.reset()
+    }
+
+    resetValidation () {
+      this.$refs.form.resetValidation()
+    }
+  }
+</script>
+
+<style scoped>
+.container {
+    width: 40%;
+    height: auto;
+}
+
+.btn_primary {
+  background: #E31F26!important;
+  color: white!important;
+  font-size: 18px!important;
+  font-weight: normal;
+  border-color: #E31F26!important;
+  text-transform: none;
+  letter-spacing: normal;
+  height: 50px!important;
+  padding: 0 20px!important;
+  margin-bottom: 16px;
+}
+.input-form {
+  height: 50px;
+  background: #FFFFFF;
+  box-shadow: 0px 0px 10px rgba(33, 51, 66, 0.25);
+  border-radius: 10px;
+  padding-bottom: 30px !important;
+  border-color: #fff !important;
+  }
+
+</style>
---
+++ pages/auth.vue
@@ -0,0 +1,106 @@
+<template>
+  <v-app>
+    <v-container>
+      <v-row>
+        <v-col cols="12" sm="8" md="6" class="mx-auto card-text" height="fit-content">
+          <v-card>
+            <v-tabs
+              v-model="tab"
+              background-color="#F9F9FA"
+              dark
+              color="#E31F26"
+            >
+              <v-tab width="50%" color="#E31F26" class="tab-sign-in">
+                Sign in
+              </v-tab>
+              <v-tab width="50%" color="#E31F26" class="tab-sign-up">
+                Sign Up
+              </v-tab>
+            </v-tabs>
+
+            <v-tabs-items v-model="tab">
+              <v-tab-item>
+                <v-card flat>
+                  <v-card-text class="card-text">
+                    <login-component />
+                  </v-card-text>
+                </v-card>
+              </v-tab-item>
+              <v-tab-item>
+                <v-card flat>
+                  <v-card-text class="card-text">
+                    <register-component />
+                  </v-card-text>
+                </v-card>
+              </v-tab-item>
+            </v-tabs-items>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+import { Vue, Component } from 'nuxt-property-decorator'
+import RegisterComponent from '../components/RegisterComponent'
+import LoginComponent from '../components/LoginComponent'
+
+export default @Component({
+  components: { LoginComponent, RegisterComponent }
+})
+
+class Auth extends Vue {
+  tab = null;
+}
+</script>
+<style scoped>
+div.v-tabs-slider{
+  color: #E31F26 !important;
+}
+.card-text {
+  height: fit-content !important;
+}
+div.v-application--wrap {
+  min-height: 0vh !important;
+  height: 500px !important;
+}
+div.v-card {
+  height: 500px !important;
+  border-radius: 30px 10px 30px 30px !important;
+  box-shadow: 0px 0px 10px rgba(33, 33, 33, 0.25) !important;
+}
+div.v-card--flat {
+  border-radius: 0px !important;
+}
+.v-card__subtitle, .v-card__text, .v-card__title {
+  padding-right: 50px !important;
+  padding-left: 50px !important;
+}
+.v-card > *:last-child:not(.v-btn):not(.v-chip):not(.v-avatar) {
+  border-bottom-left-radius: 0px;
+  border-bottom-right-radius: 0px;
+}
+.v-tab {
+  color:#213342 !important;
+  width: 100% !important;
+ }
+ .v-tab--active.v-tab:nth-child(1):not(:focus)::before {
+  border-radius: 30px 0px 0px 0px;
+}
+ .v-tab--active.v-tab:nth-child(3):not(:focus)::before {
+  border-radius: 0px 10px 0px 0px;
+}
+.v-tab:nth-child(2):before {
+  border-radius: 30px 0px 0px 0px;
+}
+.v-tab:nth-child(3):before {
+  border-radius: 0px 10px 0px 0px;
+}
+.tab-sign-in {
+  border-radius: 30px 0px 0px 0px !important;
+ }
+.tab-sign-up {
+  border-radius: 0px 10px 0px 0px !important;
+ }
+</style>
