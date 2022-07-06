12bd2f12cf44e5a5d3248c3d24a2926162347de9
Dmytro Iudin <dmtry.iudin@gmail.com>
2022-07-06 21:26:26 +0300

    news component changed

--- components/IndexPage/News/News.vue
+++ components/IndexPage/News/News.vue
@@ -1,16 +1,20 @@
 <template>
   <div class="news-wrapper">
-    <h3>Latest News</h3>
-    <NewsItem />
+    <h3 id="news-header">
+      Latest News
+    </h3>
+    <NewsItem class="d-none d-md-flex" news-item-title="Article" news-item-article="On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains." />
+    <NewsItemMobile class="d-md-none" news-item-title="Article" news-item-article="On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains." />
   </div>
 </template>
 
 <script>
 import { Component, Vue } from 'nuxt-property-decorator';
 import NewsItem from './NewsItem.vue';
+import NewsItemMobile from './NewsItemMobile.vue';
 
 export default @Component({
-  components: { NewsItem }
+  components: { NewsItem, NewsItemMobile }
 })
 
 class News extends Vue {}
--- components/IndexPage/News/NewsItem.vue
+++ components/IndexPage/News/NewsItem.vue
@@ -1,84 +1,54 @@
 <template>
   <div>
     <v-card
-      class="d-none d-md-flex d-align-center justify-space-between card"
+      class="d-flex d-align-center justify-space-between card"
       height="400px"
+      :class="{showAllContent : showAllContent}"
     >
       <v-img
         src="https://media.istockphoto.com/photos/for-rent-sign-in-front-of-new-house-picture-id149060607?k=20&m=149060607&s=612x612&w=0&h=To8zlQbIQ66ep7g-C-19aAFi2CdK98sFnLdSWL8zrl4="
         max-width="45%"
         height="100%"
+        class="news-item-img"
       />
       <div class="news-item-text d-flex align-start flex-column justify-space-around">
         <h4>{{ newsItemTitle }}</h4>
         <div class="article-content">
           <p>{{ newsItemArticle }}</p>
         </div>
-        <v-btn color="#E31F26">
-          <span class="btn-text">Details</span>
+        <v-btn
+          text
+          color="#E31F26"
+          @click="()=>{
+            showAllContent = !showAllContent
+            scroll ()
+            return null
+          }"
+        >
+          <span class="btn-text">{{ showAllContent ? 'Hide' : 'Details' }}</span>
         </v-btn>
       </div>
-    </v-card>
-    <v-card
-      class="mx-auto d-md-none"
-      max-width="100%"
-    >
-      <v-img
-        src="https://media.istockphoto.com/photos/for-rent-sign-in-front-of-new-house-picture-id149060607?k=20&m=149060607&s=612x612&w=0&h=To8zlQbIQ66ep7g-C-19aAFi2CdK98sFnLdSWL8zrl4="
-        height="400px"
-        width="100%"
-      />
-
-      <v-card-title>
-        {{ newsItemTitle }}
-      </v-card-title>
-
-      <v-card-subtitle>
-        {{ newsItemArticle.slice(0, 150)+"..." }}
-      </v-card-subtitle>
-
-      <v-card-actions>
-        <v-btn
-          color="#E31F26"
-          text
-        >
-          Details
-        </v-btn>
-
-        <v-spacer />
-
-        <v-btn
-          icon
-          @click="show = !show"
-        >
-          <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
-        </v-btn>
-      </v-card-actions>
-      <v-expand-transition>
-        <div v-show="show">
-          <v-divider />
-
-          <v-card-text>
-            {{ newsItemArticle }}
-          </v-card-text>
-        </div>
-      </v-expand-transition>
     </v-card>
   </div>
 </template>
 
 <script>
-import { Component, Vue } from 'nuxt-property-decorator';
+import { Component, Vue, Prop } from 'nuxt-property-decorator';
 
 export default @Component
 
 class NewsItem extends Vue {
+  @Prop({ type: String, required: true }) newsItemTitle
+  @Prop({ type: String, required: true }) newsItemArticle
+
   data () {
     return {
-      newsItemTitle: 'Lorem ipsum dolor sit amet',
-      newsItemArticle: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse quos molestias soluta odit accusamus qui fugit voluptatem tempore? Ratione totam nulla accusamus consectetur dicta tenetur odit minus nesciunt eaque mollitia. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga molestias odit eaque at aspernatur id quas voluptatum obcaecati incidunt in et autem ullam ducimus sit, architecto molestiae mollitia sed asperiores.',
-      show: false
+      showAllContent: false
     };
+  }
+
+  scroll () {
+    document.getElementById('news-header').scrollIntoView();
   }
 }
 </script>
@@ -95,6 +65,23 @@ class NewsItem extends Vue {
   .card{
     border: none !important;
     box-shadow: none !important;
+
+    transition: height 0.6s;
+  }
+
+  .showAllContent{
+    height: 90vh !important;
+
+    flex-direction: column;
+    .news-item-text{
+      height: 70% !important;
+      min-width: 100% !important;
+    }
+    .news-item-img{
+      height: 25% !important;
+      min-width: 100% !important;
+      margin-bottom: 5% !important;
+    }
   }
 
   .btn-text{
@@ -105,13 +92,27 @@ class NewsItem extends Vue {
     line-height: 22px;
 
     text-align: center;
-
-    color: #FFFFFF;
   }
 
   .article-content{
     height: 200px;
     overflow: scroll;
     overflow-x: hidden;
+  }
+  /* width */
+  ::-webkit-scrollbar {
+    width: 10px;
+  }
+  /* Track */
+  ::-webkit-scrollbar-track {
+    background: #dedede;
+  }
+  /* Handle */
+  ::-webkit-scrollbar-thumb {
+    background: #888;
+  }
+  /* Handle on hover */
+  ::-webkit-scrollbar-thumb:hover {
+    background: #555;
   }
 </style>
