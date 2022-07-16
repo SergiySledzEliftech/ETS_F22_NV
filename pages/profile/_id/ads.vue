fa493a348673762e90f5ae3eaeddc8e965f530c6
Marinessa393 <futurism1303@gmail.com>
2022-07-16 12:37:12 +0300

    merge previous changes

--- pages/profile/_id/ads.vue
+++ pages/profile/_id/ads.vue
@@ -1,89 +1,84 @@
 <template>
   <div>
-    <ListViewButtons :model-value="toggleDisplaying" @update="changeView" />
-
-    <items-list :is-list="toggleDisplaying === 'list'" :list="list">
-      <div class="buttons">
-        here will be buttons
-      </div>
-    </items-list>
+    <ItemsList :list="dataAds">
+      <li v-for="item in dataAds" :key="item.id" class="item">
+        <single-item :item="item" :grid="view === 'list'">
+          <div class="buttons">
+            <v-tooltip bottom>
+              <template #activator="{on, attrs}">
+                <v-btn v-bind="attrs" icon small v-on="on" @click.prevent="">
+                  <v-icon :class="view">
+                    mdi-pencil
+                  </v-icon>
+                </v-btn>
+              </template>
+              <span>Edit</span>
+            </v-tooltip>
+            <v-tooltip bottom>
+              <template #activator="{on,attrs}">
+                <v-btn v-bind="attrs" icon small v-on="on" @click.prevent="$commit('changeView', item.id)">
+                  <v-icon :class="view">
+                    mdi-trash-can
+                  </v-icon>
+                </v-btn>
+              </template>
+              <span>Delete</span>
+            </v-tooltip>
+          </div>
+        </single-item>
+      </li>
+    </ItemsList>
   </div>
 </template>
 
 <script>
-import { Vue, Component } from 'nuxt-property-decorator'
-import ItemsList from '~/components/list/list'
-import ListViewButtons from '~/components/list/displayingButtons'
-import ListItem from '~/components/list/item'
+import { Vue, Component, namespace } from 'nuxt-property-decorator'
+import ItemsList from '~/components/list/ItemsList.vue'
+import SingleItem from '~/components/list/SingleItem.vue'
+const { State, Mutation } = namespace('profile')
 
 export default @Component({
-  name: 'ads',
-  layout: 'profile',
-  components: { ItemsList, ListViewButtons, ListItem }
+  name: 'profile-ads',
+  components: { ItemsList, SingleItem }
 })
-
-class Ads extends Vue {
-  toggleDisplaying = 'grid';
-  list= [
-    {
-      id: 1,
-      title: 'Some useful title 1',
-      price: 300,
-      location: 'https://th.bing.com/th/id/R.d9b27dfdff5a2a8182304ed921f7fe67?rik=iH9rCXLTvhfxYg&pid=ImgRaw&r=0',
-      img: 'https://th.bing.com/th/id/R.d9b27dfdff5a2a8182304ed921f7fe67?rik=iH9rCXLTvhfxYg&pid=ImgRaw&r=0',
-      term: 5,
-      user: {
-        userId: 123,
-        avatar: 'https://th.bing.com/th/id/OIP.boU0VLtfyLfKIbp_1YIWJgHaFj?pid=ImgDet&rs=1',
-        firstName: 'Some',
-        lastName: 'User'
-      },
-      rating: 4
-    },
-    {
-      id: 2,
-      title: 'Some useful title 2',
-      price: 400,
-      location: 'https://th.bing.com/th/id/R.d9b27dfdff5a2a8182304ed921f7fe67?rik=iH9rCXLTvhfxYg&pid=ImgRaw&r=0',
-      img: 'https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300',
-      term: 3,
-      user: {
-        userId: 12,
-        avatar: 'https://img-19.commentcamarche.net/cI8qqj-finfDcmx6jMK6Vr-krEw=/1500x/smart/b829396acc244fd484c5ddcdcb2b08f3/ccmcms-commentcamarche/20494859.jpg',
-        firstName: 'Some',
-        lastName: 'Another User'
-      },
-      rating: 4.5
-    },
-    {
-      id: 3,
-      title: 'Some useful title 3',
-      price: 600,
-      location: 'https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300',
-      img: 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg',
-      term: 2,
-      user: {
-        userId: 12,
-        avatar: 'https://images.ctfassets.net/hrltx12pl8hq/qGOnNvgfJIe2MytFdIcTQ/429dd7e2cb176f93bf9b21a8f89edc77/Images.jpg',
-        firstName: 'Some',
-        lastName: 'Unknown User'
-      },
-      rating: 4.7
-    }
-
-  ];
-
-  changeView = function (value) {
-    this.toggleDisplaying = value
-  }
+class ProfileAds extends Vue {
+  @State dataAds
+  @State view
+  @Mutation deleteItem
 }
+
 </script>
 
-<style scoped lang="scss">
+<style lang="scss" scoped>
 .buttons {
-  padding: 5px;
+  padding: 20px;
   text-align: center;
-  margin-top: 10px;
-  border: 1px dashed rgba($light, 0.5);
+  position: absolute;
+  top: 0;
+  right: 0;
+  button {
+    margin-left: 5px;
+  }
+  i.list {
+    color: $secondary
+  }
+  i.grid {
+    color: $bg
+  }
+  i:hover, i:focus {
+    color: $light
+  }
 }
+
+.isGrid .buttons {
+  padding: 20px;
+  top: calc(100% - 68px);
+  @media (max-width: 599px) {
+    display: flex;
+    flex-direction: column;
+    top: calc(100% - 68px);
+    padding: 5px
+  }
+}
+
 </style>
--- pages/profile/_id/favorites.vue
+++ pages/profile/_id/favorites.vue
@@ -1,27 +1,47 @@
 <template>
   <div>
-    <ItemsList :list="fav" />
+    <items-list :list="fav">
+      <li v-for="item in fav" :key="item.id" class="item">
+        <single-item :item="item" :grid="view === 'list'">
+          <div class="btn">
+            <v-tooltip bottom>
+              <template #activator="{on, attrs}">
+                <v-btn fab class="btn" small v-bind="attrs" v-on="on">
+                  <v-icon color="error">
+                    mdi-heart
+                  </v-icon>
+                </v-btn>
+              </template>
+              <span>Remove from favorites</span>
+            </v-tooltip>
+          </div>
+        </single-item>
+      </li>
+    </items-list>
   </div>
 </template>
 
 <script>
-import { Vue, Component } from 'nuxt-property-decorator'
-import ItemsList from '~/components/list/list.vue'
+import { Vue, Component, namespace } from 'nuxt-property-decorator'
+import ItemsList from '~/components/list/ItemsList.vue'
+import SingleItem from '~/components/list/SingleItem.vue'
+const { State } = namespace('profile')
 
 export default @Component({
   name: 'Favorites',
   layout: 'profile',
-  components: { ItemsList }
+  components: { ItemsList, SingleItem }
 })
 
 class Favorites extends Vue {
+  @State view
   fav = [
     {
       id: 2,
       title: 'Some useful title 1',
       price: 300,
       location: 'https://th.bing.com/th/id/R.d9b27dfdff5a2a8182304ed921f7fe67?rik=iH9rCXLTvhfxYg&pid=ImgRaw&r=0',
-      img: 'https://th.bing.com/th/id/R.d9b27dfdff5a2a8182304ed921f7fe67?rik=iH9rCXLTvhfxYg&pid=ImgRaw&r=0',
+      images: ['https://th.bing.com/th/id/R.d9b27dfdff5a2a8182304ed921f7fe67?rik=iH9rCXLTvhfxYg&pid=ImgRaw&r=0'],
       term: 5,
       user: {
         userId: 123,
@@ -36,7 +56,7 @@ class Favorites extends Vue {
       title: 'Some useful title 2',
       price: 400,
       location: 'https://th.bing.com/th/id/R.d9b27dfdff5a2a8182304ed921f7fe67?rik=iH9rCXLTvhfxYg&pid=ImgRaw&r=0',
-      img: 'https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300',
+      images: ['https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300'],
       term: 3,
       user: {
         userId: 12,
@@ -52,5 +72,9 @@ class Favorites extends Vue {
 </script>
 
 <style scoped lang="scss">
-
+.btn {
+  position: absolute;
+  top: -5px;
+  right: -5px;
+}
 </style>
