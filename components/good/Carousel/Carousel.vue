8e9913c5b7c524295f6aa92323975b226f311004
OlegVBel <olegbelous2002@gmail.com>
2022-07-25 11:24:58 +0300

    saving 2

--- components/good/Carousel/Carousel.vue
+++ components/good/Carousel/Carousel.vue
@@ -42,8 +42,10 @@ export default @Component({
 class Carousel extends Vue {
   slides = [
     'https://picsum.photos/id/1032/900/400',
-    'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
-    'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg'
+    'https://picsum.photos/id/1033/900/400',
+    'https://picsum.photos/id/1037/900/400',
+    'https://picsum.photos/id/1035/900/400',
+    'https://picsum.photos/id/1036/900/400'
   ]
 
   currentSlide = 0
