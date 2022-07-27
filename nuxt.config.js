a65dfd4d0922f9f0ae5b490cf5288084edac5fb7
Serhii <havryliuk.serhii@icloud.com>
2022-07-27 21:39:40 +0300

    change baseUrl in nuxtconfig - https://glomare.herokuapp.com/

--- nuxt.config.js
+++ nuxt.config.js
@@ -62,7 +62,8 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    baseURL: 'http://localhost:3001'
+    baseURL: 'https://glomare.herokuapp.com/'
+    // baseURL: 'http://localhost:3001'
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
   },
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
