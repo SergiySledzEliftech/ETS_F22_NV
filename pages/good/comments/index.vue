08743436a35f95ab5c16a6a801a34f4c32e0893f
OlegVBel <olegbelous2002@gmail.com>
2022-07-27 18:35:09 +0300

    global progress comp

--- pages/good/comments/index.vue
+++ pages/good/comments/index.vue
@@ -12,11 +12,11 @@
 </template>
 
 <script>
-import { Vue, Component, namespace } from 'nuxt-property-decorator'
-import CommentList from '~/components/good/CommentList'
-import ProgressCircular from '~/components/good/Progress'
+import { Vue, Component, namespace } from 'nuxt-property-decorator';
+import CommentList from '~/components/good/CommentList';
+import ProgressCircular from '~/components/global/Progress';
 
-const { State, Action } = namespace('good_comments')
+const { State, Action } = namespace('good_comments');
 
 export default @Component({
   components: {
@@ -34,15 +34,15 @@ class AllComments extends Vue {
   // also can be used fetch
   async mounted () {
     try {
-      await this.loadComments()
+      await this.loadComments();
     } catch (err) {
       // eslint-disable-next-line no-console
-      console.error(err.message)
+      console.error(err.message);
     }
   }
 
   created () {
-    this.setLoading(false)
+    this.setLoading(false);
   }
 }
 </script>
