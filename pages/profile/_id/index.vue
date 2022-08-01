ba09a529cfc442ab23165e56260bd36079662826
Kak2z <kak2z0079803@gmail.com>
2022-08-01 21:22:25 +0300

    minor improvement after lesson

--- pages/profile/_id/index.vue
+++ pages/profile/_id/index.vue
@@ -724,10 +724,8 @@ class Config extends Vue {
 
       .avtr-edit {
         position: absolute;
-        top: 0;
-        bottom: 0;
-        right: 0;
-        left: 0;
+        top: 15px;
+        right: 15px;
         color: $secondary;
 
         &:hover {
