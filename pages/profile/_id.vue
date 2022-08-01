b49c17411024122fdb70b48a537771646f2c9ca0
m.vyshnevska <m.vyshnevska@spd-ukraine.com>
2022-08-02 02:14:05 +0300

    fix wrapper styles

--- pages/profile/_id.vue
+++ pages/profile/_id.vue
@@ -80,6 +80,16 @@ class Profile extends Vue {
 
 <style lang="scss">
 .profile-wrap {
+  width: 100%;
+  margin-left: auto;
+  margin-right: auto;
+  @media (min-width: 960px) {
+  max-width: 900px
+  }
+
+  @media (min-width: 1264px) {
+    max-width: 1185px;
+  }
 
   .row {
     margin: 10px 0;
